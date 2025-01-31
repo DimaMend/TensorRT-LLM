e6c9695858d14d9faa33685407e1c3610949f24d
scarborough high school <tsnyder@scarboroughschools.org>
2025-01-31 12:15:57 -0500

    add chunked context/prefill runtime option to trtllm-serve

--- tensorrt_llm/commands/serve.py
+++ tensorrt_llm/commands/serve.py
@@ -20,6 +20,10 @@ from tensorrt_llm.serve import OpenAIServer
               default="localhost",
               help="Hostname of the server.")
 @click.option("--port", type=int, default=8000, help="Port of the server.")
+@click.option("--backend",
+              type=click.Choice(["pytorch"]),
+              default=None,
+              help="Set to 'pytorch' for pytorch path. Default is cpp path.")
 @click.option("--max_beam_width",
               type=int,
               default=BuildConfig.max_beam_width,
@@ -34,6 +38,11 @@ from tensorrt_llm.serve import OpenAIServer
     default=BuildConfig.max_num_tokens,
     help=
     "Maximum number of batched input tokens after padding is removed in each batch."
+)
+@click.option("--chunked_context",
+              is_flag=True,
+              default=False,
+              help="Enable chunked context (chunked prefill)."
 )
 @click.option(
     "--max_seq_len",
@@ -55,10 +64,11 @@ from tensorrt_llm.serve import OpenAIServer
               is_flag=True,
               default=False,
               help="Flag for HF transformers.")
-def main(model: str, tokenizer: str, host: str, port: int, max_beam_width: int,
-         max_batch_size: int, max_num_tokens: int, max_seq_len: int,
-         tp_size: int, pp_size: int, kv_cache_free_gpu_memory_fraction: float,
-         trust_remote_code: bool):
+def main(model: str, tokenizer: str, host: str, port: int, backend: str,
+         max_beam_width: int, max_batch_size: int, max_num_tokens: int,
+         max_seq_len: int, tp_size: int, pp_size: int,
+         kv_cache_free_gpu_memory_fraction: float,
+         trust_remote_code: bool, chunked_context: bool):
     """Running an OpenAI API compatible server
 
     MODEL: model name | HF checkpoint path | TensorRT engine path
@@ -79,7 +89,8 @@ def main(model: str, tokenizer: str, host: str, port: int, max_beam_width: int,
         trust_remote_code=trust_remote_code,
         build_config=build_config,
         kv_cache_config=kv_cache_config,
-    )
+        enable_chunked_prefill=chunked_context,
+        backend=backend if backend == "pytorch" else None)
 
     llm = LLM(**llm_args.to_dict())
 
